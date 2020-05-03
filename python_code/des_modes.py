628426082bf12d4b9f9e75bb8d7da337f8758398
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2020-05-03 06:33:04 +0200

    split message into blocks and pad it

--- python_code/des_modes.py
+++ python_code/des_modes.py
@@ -2,14 +2,22 @@ class BlockMode:
     def __init__(self):
         pass
     
-    def splitAndPad(self):
-        pass
+    def splitAndPad(self,msg,blockSize=8):
+        print(len(msg))
+        padLen = blockSize - len(msg)%blockSize
+        print(padLen)
+        msg += padLen * '_'
+        blocks=[]
+        print(len(msg))
+        for i in range (0,len(msg),blockSize):
+            blocks.append(msg[i : i+ blockSize])
+            print(msg[i : i+blockSize])
 
     def encrypt(self,plainText):
         pass
     def decrypt(self,cipherText):
         pass
-        
+   
 class ECBBlockMode(BlockMode):
 
     def encrypt(self,plainText):
