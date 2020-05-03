cebb451f84c643a48bc1e4bab0f0098023e11144
hagarhaytham <hagar.bahy97@eng-st.cu.edu.eg>
2020-05-03 05:32:33 +0200

    Server side functions

--- python_code/sender.py
+++ python_code/sender.py
@@ -2,19 +2,50 @@ import zmq
 import random
 import sys
 import time
+import os
+import errno
+import des_modes
 
-block_mode = 1
+def readInfo(filepath):
+    if not os.path.isfile(filepath):
+        raise FileNotFoundError(errno.ENOENT, os.strerror(errno.ENOENT), filepath)
+    with open (filepath) as f:
+        lines =f.readlines()
+    blockMode = int(lines[0])
+    plainTextMsg = lines[1]
+    return blockMode, plainTextMsg
 
-port = "5556"
-context = zmq.Context()
-socket = context.socket(zmq.PAIR)
-socket.bind("tcp://*:%s" % port)
+def makeConnection(port ="5556"):
+    context = zmq.Context()
+    socket = context.socket(zmq.PAIR)
+    socket.bind("tcp://*:%s" % port)
+    return socket
 
-print("Sending Block Mode No. ",block_mode)
-socket.send_string(str(block_mode))
+def getBlockMode(mode):
+    ModeOfOperation = None
+    if mode ==1:
+        ModeOfOperation = ECBBlockMode()
+    elif mode ==2:
+        ModeOfOperation = CBCBlockMode()
+    elif mode ==3:
+        ModeOfOperation = CFCBlockMode()
+    else:
+        ModeOfOperation = CTRBlockMode()
+    return ModeOfOperation
 
-# while True:
-#     socket.send_string("Server message to client3")
-#     msg = socket.recv()
-#     print (msg)
-#     time.sleep(1)
+def sendBlockMode(socket,mode):
+    print("Sending Block Mode No. ",mode)
+    socket.send_string(str(mode))
+
+def sendCipheredText(socket,msg):
+    print("Sending Ciphered message : ",msg)
+    socket.send_string(str(msg))
+
+if __name__ =="__main__":
+    blockMode,plainTextMsg = readInfo('../testcases/1.txt')
+    socket = makeConnection()
+    print("Plain Text: ",plainTextMsg)
+    ModeOfOperation = getBlockMode(blockMode)
+    messageBlocks= ModeOfOperation.splitAndPad(plainTextMsg)
+    cipherText=ModeOfOperation.encrypt(messageBlocks)
+    sendCipheredText(socket,cipherText)
